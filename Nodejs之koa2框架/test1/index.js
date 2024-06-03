3521f3a101c9035fa00ccc1ccaa7f4d02e8363a5
xiaoxiaoyan1223 <97817462+xiaoxiaoyan1223@users.noreply.github.com>
2024-06-03 17:39:58 +0800

    Update index.js

--- Nodejs之koa2框架/test1/index.js
+++ Nodejs之koa2框架/test1/index.js
@@ -1,7 +1,9 @@
 const Koa=require('koa')  //require是commonjs模块化规范  包含三个层级
 const app=new Koa()
-// cte=>context  上下文
+// ctx=>context  上下文
 app.use(async()=>{
     ctx.body='hello world'
 })
 app.listen(3000) //web server监听3000端口
+
+// 通过node index.js启动服务器
